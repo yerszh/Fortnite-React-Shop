7bbe4d8a1002498a9211db18409d58cdb6f3d620
Yerszh <yers.zh@gmail.com>
2022-08-26 02:08:39 +0600

    added gh-pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,12 @@ function Footer() {
     <div className="footer-copyright">
       <div className="container">
       © {new Date().getFullYear()} Copyright Text
-      <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+      <a 
+        className="grey-text text-lighten-4 right" 
+        href="https://yerszh.github.io/Fortnite-React-Shop/" 
+        target="_blank" 
+        rel="noreferrer"
+      >Repo</a>
       </div>
     </div>
   </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 function Header() {
     return <nav className="blue-grey lighten-1">
         <div className="nav-wrapper">
-        <a href="#" className="brand-logo">Fortnite React Shop</a>
+        <a href="/" className="brand-logo">Fortnite React Shop</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="!#">Repo</a></li>
+            <li><a href="https://yerszh.github.io/Fortnite-React-Shop/" target="_blank" rel="noreferrer">Repo</a></li>
         </ul>
         </div>
     </nav>
